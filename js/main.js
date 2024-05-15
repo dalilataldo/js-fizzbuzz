49340381f75b0c2cf1dbe5dafd22de4530c18808
dalilataldo <dalila.taldo@gmail.com>
2024-05-15 13:32:12 +0200

    add: if i %3 && i%5

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
-// valore start
-let start = 1
+// valori di partenza
+let x = 1
 
 // ciclo for
-for (let i = start; i <= 100; i++) {
-    if (i % 3 === 0) { console.log('Buzz'); }
-    else if (i % 5 === 0) { console.log('Fitz'); }
-    // else if (i % 3 and % 5 === 0) { console.log('Fitz'); }
+for (let i = x; i <= 100; i++) {
+    if (i % 3 === 0 && i % 5 === 0) { console.log('FizzBuzz'); }
+    else if (i % 3 === 0) { console.log('Fizz'); }
+    else if (i % 5 === 0) { console.log('Buzz'); }
     else { console.log(i); }
 }
